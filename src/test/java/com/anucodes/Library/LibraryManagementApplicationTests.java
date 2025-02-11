b20119deeaee4a5585995b2485e5665e7445b182
Anubhav Singh <86763919+Anubhav8176@users.noreply.github.com>
2025-02-11 08:54:38 +0530

    Initialization

---
+++ src/test/java/com/anucodes/Library/LibraryManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anucodes.Library;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
