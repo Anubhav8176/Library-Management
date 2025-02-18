8416e821be20c5c71af7df112c6492085bdf04df
Anubhav Singh <86763919+Anubhav8176@users.noreply.github.com>
2025-02-18 21:23:02 +0530

    Book Controller

--- src/main/java/com/anucodes/Library/controller/BookController.java
+++ src/main/java/com/anucodes/Library/controller/BookController.java
@@ -1,12 +1,15 @@
 package com.anucodes.Library.controller;
 
 
+import com.anucodes.Library.entity.Book;
 import com.anucodes.Library.model.BookDto;
 import com.anucodes.Library.services.BookServices;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("v1/book")
@@ -25,6 +28,33 @@ public class BookController {
         }
     }
 
+    @GetMapping("/getAll")
+    public ResponseEntity getAllBooks(){
+        try{
+            List<BookDto> books = bookServices.getAllBooks();
+            return new ResponseEntity<>(books, HttpStatus.ACCEPTED);
+        } catch (RuntimeException e) {
+            throw new RuntimeException(e);
+        }
+    }
 
+    @GetMapping("/getByAuthor")
+    public ResponseEntity getAllByAuthor(@RequestBody String author){
+        try{
+            List<BookDto> books = bookServices.getAllBooksByAuthor(author);
+            return new ResponseEntity<>(books, HttpStatus.ACCEPTED);
+        } catch (RuntimeException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @DeleteMapping("/deleteBook")
+    public ResponseEntity deleteBook(@RequestBody String isbn){
+        try {
+            return new ResponseEntity<>("Book deleted successfully!", HttpStatus.OK);
+        } catch (RuntimeException e) {
+            throw new RuntimeException(e);
+        }
+    }
 
 }
